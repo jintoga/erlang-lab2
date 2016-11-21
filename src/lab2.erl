70d17bfbc0dd23d86d7bb64954ec2e19032c68ce
Dat <jintoga123@yahoo.com>
2016-11-21 15:24:10 +0700

    working on "archer"

--- src/lab2.erl
+++ src/lab2.erl
@@ -1,6 +1,6 @@
 %% @author DAT 
 -module(lab2). 
--export([start/0, make_request/2, getAttackedBy/3, game/0, tanker/1, boss/1 ]).
+-export([start/0, make_request/2, getAttackedBy/3, game/0, tanker/1, boss/1, archer/1 ]).
  
 
 make_request(Id, Msg) -> Id ! Msg.
@@ -22,30 +22,47 @@ tanker(0) -> getAttackedBy(0, "Tanker", "Boss"),
 			 make_request(boss_pid, tanker_dead);
 
 tanker(TANKER_HP) ->
-	io:format("~nTanker(~w): wating", [TANKER_HP]), 
-	make_request(boss_pid, get_attacked_by_tanker),
+	io:format("~nTanker(~w): wating", [TANKER_HP]),  
 	receive  
 		game_started -> io:format("~nTanker: Game started! Attacking Boss", []),
 						make_request(boss_pid, get_attacked_by_tanker);
-		get_attacked_by_boss -> getAttackedBy(TANKER_HP, "Tanker", "Boss")
+		get_attacked_by_boss -> getAttackedBy(TANKER_HP, "Tanker", "Boss"),
+								io:format("~nTanker(~w): Calling archer!", [TANKER_HP]),
+								make_request(archer_pid, tanker_called)
 	end,
 	tanker(TANKER_HP - 10).		
   
 boss(0) -> io:format("~nBoss: I'm dead. Game Over!", []);
 	
 boss(BOSS_HP) -> 
+	io:format("~nBoss(~w): wating", [BOSS_HP]),
 	receive  
 		tanker_dead ->  io:format("~nGame Over! Boss Won", []);
 		get_attacked_by_tanker ->  getAttackedBy(BOSS_HP, "Boss", "Tanker"),
 								   make_request(tanker_pid, get_attacked_by_boss),
+								   boss(BOSS_HP - 10);
+		get_attacked_by_archer ->  getAttackedBy(BOSS_HP, "Boss", "Archer"),
+								   make_request(archer_pid, get_attacked_by_boss),
 								   boss(BOSS_HP - 10)
-	end,
-	io:format("~nBoss(~w): wating", [BOSS_HP]).
+	end.
 
+archer(0) -> getAttackedBy(0, "Archer", "Boss");
+
+archer(ARCHER_HP) -> 
+	io:format("~nArcher(~w): wating", [ARCHER_HP]),
+	make_request(boss_pid, get_attacked_by_archer),
+	receive  
+		tanker_called -> io:format("~nArcher(~w): Tanker called! Attacking Boss", [ARCHER_HP]),
+						 make_request(boss_pid, get_attacked_by_archer);
+		get_attacked_by_boss -> getAttackedBy(ARCHER_HP, "Archer", "Boss") 
+	end,
+	tanker(ARCHER_HP - 10).
+ 
 start() ->  
 	register(game_pid, spawn(lab2, game, [])), 
-	register(boss_pid, spawn(lab2, boss, [50])), 
-	register(tanker_pid, spawn(lab2, tanker, [30])),
+	register(boss_pid, spawn(lab2, boss, [80])), 
+	register(tanker_pid, spawn(lab2, tanker, [50])), 
+	register(archer_pid, spawn(lab2, archer, [10])),
     make_request(game_pid, game_started).
 	
  
