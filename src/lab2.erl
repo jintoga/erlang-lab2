eae5fe4cb1e3c0c435e13be46d458f12a9dfdbfb
Dat <jintoga123@yahoo.com>
2016-11-23 18:53:49 +0700

    fixed

--- src/lab2.erl
+++ src/lab2.erl
@@ -1,6 +1,6 @@
 %% @author DAT 
 -module(lab2). 
--export([start/0, make_request/2, getAttackedBy/3, game/0, tanker/1, boss/1, archer/1 ]).
+-export([start/0, make_request/2, getAttackedBy/3, game/0, tanker/1, boss/1, archer/1]).
  
 
 make_request(Id, Msg) -> Id ! Msg.
@@ -25,12 +25,13 @@ tanker(TANKER_HP) ->
 	io:format("~nTanker(~w): wating", [TANKER_HP]),  
 	receive  
 		game_started -> io:format("~nTanker: Game started! Attacking Boss", []),
-						make_request(boss_pid, get_attacked_by_tanker);
-		get_attacked_by_boss -> getAttackedBy(TANKER_HP, "Tanker", "Boss"),
-								io:format("~nTanker(~w): Calling archer!", [TANKER_HP]),
-								make_request(archer_pid, tanker_called)
-	end,
-	tanker(TANKER_HP - 10).		
+						make_request(boss_pid, get_attacked_by_tanker),
+						tanker(TANKER_HP); 
+		get_attacked_by_boss -> TANKER_HP_NEW = TANKER_HP - 10,
+								getAttackedBy(TANKER_HP_NEW, "Tanker", "Boss"), 
+								io:format("~nTanker(~w): Calling archer!", [TANKER_HP_NEW]),
+								make_request(archer_pid, tanker_called)  
+	end.		
   
 boss(0) -> io:format("~nBoss: I'm dead. Game Over!", []);
 	
@@ -38,26 +39,22 @@ boss(BOSS_HP) ->
 	io:format("~nBoss(~w): wating", [BOSS_HP]),
 	receive  
 		tanker_dead ->  io:format("~nGame Over! Boss Won", []);
-		get_attacked_by_tanker ->  getAttackedBy(BOSS_HP, "Boss", "Tanker"),
-								   make_request(tanker_pid, get_attacked_by_boss),
-								   boss(BOSS_HP - 10);
-		get_attacked_by_archer ->  getAttackedBy(BOSS_HP, "Boss", "Archer"),
-								   make_request(archer_pid, get_attacked_by_boss),
-								   boss(BOSS_HP - 10)
+		get_attacked_by_tanker -> BOSS_HP_NEW = BOSS_HP - 10,
+								  getAttackedBy(BOSS_HP_NEW, "Boss", "Tanker"), 
+								  io:format("~nBoss(~w): Attacking back tanker!", [BOSS_HP_NEW]),   
+								  make_request(tanker_pid, get_attacked_by_boss) 
 	end.
-
+   
 archer(0) -> getAttackedBy(0, "Archer", "Boss");
 
-archer(ARCHER_HP) -> 
-	io:format("~nArcher(~w): wating", [ARCHER_HP]),
-	make_request(boss_pid, get_attacked_by_archer),
+archer(ARCHER_HP) ->  
+	io:format("~nArcher(~w): wating", [ARCHER_HP]), 
 	receive  
 		tanker_called -> io:format("~nArcher(~w): Tanker called! Attacking Boss", [ARCHER_HP]),
 						 make_request(boss_pid, get_attacked_by_archer);
 		get_attacked_by_boss -> getAttackedBy(ARCHER_HP, "Archer", "Boss") 
-	end,
-	tanker(ARCHER_HP - 10).
- 
+	end.
+
 start() ->  
 	register(game_pid, spawn(lab2, game, [])), 
 	register(boss_pid, spawn(lab2, boss, [80])), 
